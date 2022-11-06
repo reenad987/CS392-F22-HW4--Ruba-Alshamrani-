217b68cbbcfd1f08f2dd1f25959b2aee902a8a78
RubaAlshamrani <117397038+RubaAlshamrani@users.noreply.github.com>
2022-11-06 21:43:57 +0300

    Update coda.js

--- coda.js
+++ coda.js
@@ -3,10 +3,10 @@
    <head>
       <title>Adding two numbers using JavaScript in HTML</title>
       <script>
-         var num1 = 10;
+         var nm1 = 10;
          var num2 = 40;
          var sum = num1+num2;
-         document.write("Sum of two numbers is " + sum);
+         document.write("Sum of two numbers is " + dum);
       </script>
    </head>
    <body></body>
