dc97eec67f5b399757bceb97ae185377d02b8119
dema1999 <114085298+dema1999@users.noreply.github.com>
2022-11-07 13:07:22 +0300

    Update coda.js

--- coda.js
+++ coda.js
@@ -6,7 +6,10 @@
          var num1 = 10;
          var num2 = 40;
          var sum = num1+num2;
+         var multip = num1*num2;
+         
          document.write("Sum of two numbers is " + sum);
+         document.write("Multiplication of two numbers is " + multip);
       </script>
    </head>
    <body></body>
