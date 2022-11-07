71ef211960de4844df13960772ab46695cec88ca
reenad987 <117481032+reenad987@users.noreply.github.com>
2022-11-07 13:31:14 +0300

    Update coda.js

--- coda.js
+++ coda.js
@@ -1,4 +1,4 @@
-<!doctype html>
+<!doctype html>https://github.com/reenad987/CS392-F22-HW4--Ruba-Alshamrani-/blob/main/coda.js
 <html>
    <head>
       <title>Adding two numbers using JavaScript in HTML</title>
@@ -7,9 +7,11 @@
          var num2 = 40;
          var sum = num1+num2;
          var multip = num1*num2;
+         var sub = num1 - num2;
          
          document.write("Sum of two numbers is " + sum);
          document.write("Multiplication of two numbers is " + multip);
+         document.write("Subtraction of two numbers is " + sub);
       </script>
    </head>
    <body></body>
