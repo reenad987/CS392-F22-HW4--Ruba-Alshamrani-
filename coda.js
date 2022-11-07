9e2dafff328ecc5620fdbb3a6a6f6447a247d0b6
reenad987 <117481032+reenad987@users.noreply.github.com>
2022-11-07 13:16:00 +0300

    Update coda.js

--- coda.js
+++ coda.js
@@ -7,9 +7,13 @@
          var num2 = 40;
          var sum = num1+num2;
          var multip = num1*num2;
-         
+         var sub = num1 - num2;
+
          document.write("Sum of two numbers is " + sum);
+
          document.write("Multiplication of two numbers is " + multip);
+
+         document.write("Subtraction of two numbers is " + sub);
       </script>
    </head>
    <body></body>
